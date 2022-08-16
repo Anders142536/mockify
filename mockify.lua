d12f49f4e00dd6c9fe22c444b885e6a2f6c19f86
Dominik Galbavy <dominik.galbavy@smarttech.at>
2022-08-16 16:56:15 +0200

    added script

---
+++ mockify.lua
@@ -0,0 +1,29 @@
+#!C:/Program Files/Lua/lua-5.4.2_Win64_bin/lua
+
+local mock = ''
+
+print('starting')
+
+for i = 1, #(arg) do
+	local word = arg[i]
+	local limit = 0.5
+	-- print(word)
+	for j = 1, string.len(word) do
+		local char = string.sub(word, j, j)
+
+		local r = math.random()
+		if r > limit then
+			char = string.lower(char)
+			limit = limit * 1.3
+		else
+			char = string.upper(char)
+			limit = limit / 1.3
+		end
+		
+		mock = mock .. char
+	end
+	mock = mock .. ' '
+end
+
+print('done, mock: \n' .. mock)
+
